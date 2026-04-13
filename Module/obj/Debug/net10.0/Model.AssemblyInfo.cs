16a97226277fedccb0a48a3dd701a4bfd170c748
LE TUNG <tungle07102005@gmail.com>
2026-04-13 10:06:18 +0700

    chore: clean up gitignore and environment configs

--- Module/obj/Debug/net10.0/Model.AssemblyInfo.cs
+++ Module/obj/Debug/net10.0/Model.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Model")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c894b954cfce5f8c75a1395deb046eaa89c51e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94bb06ee9a0a90aaa3048c047cca659748bc15ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Model")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Model")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
